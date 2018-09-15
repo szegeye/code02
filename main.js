05866d6bf4dd3e9685683751ed91562c74cfef22
szegeye <selomebzegeye@gmail.com>
2018-09-15 08:33:44 -0700

    Hi there

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log("Hello World");
+
+let box = "bottle";
+console.log(box);
+
+box = "phone";
+console.log(box);
+
+box = "wire"
+console.log(box);
+
+let city;
+city = "Addis";
+console.log(city);
+
+city = "Washington DC";
+console.log(city);
